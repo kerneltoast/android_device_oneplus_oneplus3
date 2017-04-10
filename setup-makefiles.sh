f14f73c15a81600b47ad4585bc3d41379b0152d3
Sultanxda <sultanxda@gmail.com>
2017-04-10 00:42:25 -0700

    oneplus3: Remove perf, thermal, and UI blur blobs
    These features are no longer used, so remove the blobs for them.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -59,21 +59,9 @@ cat << EOF >> "$PRODUCTMK"
 endif
 
 -include vendor/extra/devices.mk
-ifneq (\$(call is-qc-perf-target),true)
 EOF
 
 cat << EOF >> "$ANDROIDMK"
-endif
-
-ifneq (\$(TARGET_HAVE_QC_PERF),true)
-EOF
-
-write_makefiles "$MY_DIR"/proprietary-files-qc-perf.txt
-
-echo "endif" >> "$PRODUCTMK"
-
-cat << EOF >> "$ANDROIDMK"
-
 endif
 
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
