b5c6f286a7e5b7fc28dbdd6827c8e7d9487b7047
dianlujitao <dianlujitao@lineageos.org>
2017-04-17 19:16:37 +0800

    op3: Add specific camera parameter library
     * Just copypasta from fw/av at this stage.
    
    Change-Id: I6c19af76577f59445d078a5c0fc81706d736c4f0

--- camera/Android.mk
+++ camera/Android.mk
@@ -24,3 +24,17 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_32_BIT_ONLY := true
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    frameworks/av/include
+
+LOCAL_SRC_FILES := \
+    CameraParameters.cpp
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libcamera_parameters_ext
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_STATIC_LIBRARY)
